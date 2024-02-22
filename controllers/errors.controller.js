27c550128be64aad0af3184a340c66efd0459d62
Ewelina <ewelinazawol@gmail.com>
2024-02-22 12:38:35 +0000

    Update handlePSQLErrors

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,6 +1,8 @@
 function handlePSQLErrors(err, req, res, next) {
   if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "Bad request." });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Not found." });
   } else {
     next(err);
   }
