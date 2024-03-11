08484dd322565957acff4a9fde743bb27d31dc68
Ewelina Zawol <ewelina@Ewelinas-MacBook-Pro.local>
2024-03-11 10:06:21 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require('cors');
 const {
   handleCustomErrors,
   handlePSQLErrors,
@@ -7,6 +8,7 @@ const {
 } = require("./controllers/errors.controller");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
